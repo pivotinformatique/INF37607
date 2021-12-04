e15b7e3f135b4da790c82b30051d8a2efcb89aaf
michaelccote <50204891+michaelccote@users.noreply.github.com>
2021-12-04 06:50:34 -0500

    Ajout des tests unitaires

---
+++ Traitement déclarations de revenus/TDRSolutionFrontEnd/TDRSolutionFrontEnd.TestsUnitaires/Core/Entities/SubmitDeclarationRevenus.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+using Moq;
+
+using TDRSolutionFrontEnd.Core.Entities;
+using TDRSolutionFrontEnd.Core.Interfaces;
+using TDRSolutionFrontEnd.Core.Services;
+
+namespace TDRSolutionFrontEnd.TestsUnitaires.Core.Entities
+{
+    public class SubmitDeclarationRevenus
+    {
+        private Mock<IDeclarationRevenuRepository> _mockDeclarationRevenuRepository = new Mock<IDeclarationRevenuRepository>();
+        private Mock<IUserRepository> _mockUserRepository = new Mock<IUserRepository>();
+
+
+        public SubmitDeclarationRevenus()
+        {
+            _mockDeclarationRevenuRepository = new Mock<IDeclarationRevenuRepository>();
+            _mockUserRepository = new Mock<IUserRepository>();
+        }
+
+        [Fact]
+        public async void SoumettreDeclarationRevenus()
+        {
+            // Première étape: configurer les mocks pour qu’ils retournent les bons objets
+
+            Contribuable user = new Contribuable { Id = 1 };
+            _mockUserRepository.Setup(x =>
+                  x.GetByIdAsync(user.Id)).ReturnsAsync(user);
+            DeclarationRevenus declarationRevenus = new DeclarationRevenus { Id = 1 };
+            declarationRevenus.Contribuable = user;
+            _mockDeclarationRevenuRepository.Setup(x => x.GetByIdAsync(declarationRevenus.Id)).ReturnsAsync(declarationRevenus);
+
+
+
+            //Deuxième étape: appeler le service
+
+            DeclarationRevenuService service = new DeclarationRevenuService(_mockDeclarationRevenuRepository.Object, null, null);
+            await service.AddDeclarationRevenu(declarationRevenus);
+
+            //Troisième étape: vérifier les appels aux repositories:
+
+            _mockDeclarationRevenuRepository.Verify(x => x.AddAsync(declarationRevenus), Times.Once);
+            _mockDeclarationRevenuRepository.VerifyNoOtherCalls();
+
+            //Quatrième étape: vérifier la valeur finale
+
+            Assert.Equal(1, declarationRevenus.Contribuable.Id);
+        }
+
+    }
+}
