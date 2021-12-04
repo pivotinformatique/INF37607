ef6f0518054bed9cdedb6e6e0a9ebdf4bf6b6a37
michaelccote <50204891+michaelccote@users.noreply.github.com>
2021-12-04 07:26:56 -0500

    Expressions in JSX

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const name = 'Brad'
+  const x = true
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello From React {x ? 'Yes' : 'No'}</h1>
+      <h2>Hello {name} {2+2}</h2>
     </div>
+
   );
 }
 
