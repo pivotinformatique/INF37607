f66f649019deb3402153ca9d1374e4ea6313ff4e
michaelccote <50204891+michaelccote@users.noreply.github.com>
2021-12-04 11:55:46 -0500

    optional message on delete

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -26,12 +26,12 @@ import Tasks from './Components/Tasks'
 
 //Delete Trask
 const deleteTask = (id) => {
-  console.log('delete',id)
+    setTasks(tasks.filter((task) => task.id != id))
 }
   return (
     <div className="container">
       <Header/>
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/>:'No Tasks to Show'}
     </div>
 
   );
