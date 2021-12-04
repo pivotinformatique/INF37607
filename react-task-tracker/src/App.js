c3c7b32dc764fc725ba065c6408a451663962d9d
michaelccote <50204891+michaelccote@users.noreply.github.com>
2021-12-04 12:08:17 -0500

    toggle reminder

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -28,10 +28,17 @@ import Tasks from './Components/Tasks'
 const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id != id))
 }
+
+//Toggle Reminder 
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id == id ? {...task,reminder:!task.reminder} : task))
+}
+
+
   return (
     <div className="container">
       <Header/>
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/>:'No Tasks to Show'}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>:'No Tasks to Show'}
     </div>
 
   );
--- react-task-tracker/src/Components/Task.js
+++ react-task-tracker/src/Components/Task.js
@@ -1,7 +1,7 @@
 import { FaTimes } from 'react-icons/fa'
-const Task = ( {task, onDelete} ) => {
+const Task = ( {task, onDelete, onToggle} ) => {
     return (
-        <div className='task'>
+        <div className={`task ${task.reminder? 'reminder': ''}`} onDoubleClick = { () => onToggle(task.id)}>
             <h3>
                 {task.text} <FaTimes style={{ color:'red', cursor: 'pointer'}} onClick= {() => onDelete(task.id)}/>
             </h3>
@@ -9,5 +9,6 @@ const Task = ( {task, onDelete} ) => {
         </div>
     )
 }
+
 
 export default Task
