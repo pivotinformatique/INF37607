15fca11b11165006baaba12f1f6af1022c4141a4
michaelccote <50204891+michaelccote@users.noreply.github.com>
2021-12-04 11:27:43 -0500

    Events

--- react-task-tracker/src/Components/Header.js
+++ react-task-tracker/src/Components/Header.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 const Header = ({ title }) => {
+    const onClick = () =>{
+        console.log('Click')
+    }
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add'></Button>
+            <Button color='green' text='Add' onClick={onClick}></Button>
         </header>
     )
 }
