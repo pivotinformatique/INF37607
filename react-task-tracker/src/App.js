6180acd702b44c69b20ce725fe99dd8fa40f0f67
michaelccote <50204891+michaelccote@users.noreply.github.com>
2021-12-04 11:44:17 -0500

    task component

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,11 +1,32 @@
+import { useState } from 'react'
 import Header from './Components/Header'
 import Tasks from './Components/Tasks'
 
  const App = () => {
+  const[tasks,setTasks] = useState( [
+    {
+        id: 1,
+        text: 'Doctors Appointement',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+])
   return (
     <div className="container">
       <Header/>
-      <Tasks/>
+      <Tasks tasks={tasks}/>
     </div>
 
   );
--- react-task-tracker/src/Components/Tasks.js
+++ react-task-tracker/src/Components/Tasks.js
@@ -1,31 +1,11 @@
-import { useState } from 'react'
-
-const Tasks = () => {
-    const[tasks,setTasks] = useState( [
-        {
-            id: 1,
-            text: 'Doctors Appointement',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Meeting at School',
-            day: 'Feb 6th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Food shopping',
-            day: 'Feb 5th at 2:30pm',
-            reminder: false,
-        }
-    ])
+import Task from './Task'
+const Tasks = ({tasks}) => {
 
     return (
         <>
             {tasks.map((task) => (
-                <h3 key={task.id}>{task.text}</h3>))}
+                <Task key={task.id} task={task} />
+                ))}
         </>
     )
 }
