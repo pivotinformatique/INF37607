e1df115d039e3c57847356002c8daeba6ff5f758
michaelccote <50204891+michaelccote@users.noreply.github.com>
2021-12-11 12:29:35 -0500

    Avancement tutoriel

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -4,6 +4,7 @@ import Tasks from './Components/Tasks'
 import AddTask from './Components/AddTask'
 
  const App = () => {
+  const[showAddTask, setShowAddTask] = useState(false)
   const[tasks,setTasks] = useState( [
     {
         id: 1,
@@ -44,8 +45,8 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header/>
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>:'No Tasks to Show'}
     </div>
 
